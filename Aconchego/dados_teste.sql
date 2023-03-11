83ada44f8e54a72b395d3ff7fa0123b99632443f
RRonanS <94500870+RRonanS@users.noreply.github.com>
2023-03-11 16:23:56 -0300

    Update dados_teste.sql

--- Aconchego/dados_teste.sql
+++ Aconchego/dados_teste.sql
@@ -1,7 +1,7 @@
 insert into usuario values(6670061501, 'Pedro', 'bjkre@hotmail.com', '123456', 'rua 1', '99999999999');
-insert into usuario values(12345678, 'Dieguinho Fuboca', 'diego.bakugan@hotmail.com', 'rua 2', '92112534214');
-insert into usuario values(12345679, 'Pedritto', 'inutil@hotmail.com', 'rua 3', '92112534214');
-insert into usuario values(12345698, 'Joje', 'jojinho@hotmail.com', 'rua 4', '82112534214');
+insert into usuario values(12345678, 'Diego', 'diego.bakugan@hotmail.com', '123456', 'rua 2', '92112534214');
+insert into usuario values(12345679, 'Pedritto', 'pedritto@hotmail.com', '123456', 'rua 3', '92112534214');
+insert into usuario values(12345698, 'Jorge', 'jorge@hotmail.com', '123456', 'rua 4', '82112534214');
 
 insert into administrador values(6670061501);
 insert into profissional values(12345678, 6670061501, 'registro');
