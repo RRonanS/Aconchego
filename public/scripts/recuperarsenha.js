c072ee8af79e1bae95f3a29e102c6b27f3a6e5f1
RRonanS <94500870+RRonanS@users.noreply.github.com>
2023-04-24 23:20:40 -0300

    Delete public directory

--- public/scripts/recuperarsenha.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector('#form_recuperar');
-const feedback = document.querySelector('#feedback');
-const email = document.querySelector('#email');
-
-form.addEventListener('submit', (event) => {
-
-// mostra mensagem de feedback
-feedback.innerText = "Um email será enviado para "+email.value+" com as instruções para recuperação";
-});
