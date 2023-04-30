1e0beee22d1dab88377adb53d32aa5f9856ab3a0
RRonanS <94500870+RRonanS@users.noreply.github.com>
2023-04-29 21:28:59 -0300

    Update dados_teste.sql

--- Aconchego/dados_teste.sql
+++ Aconchego/dados_teste.sql
@@ -1,9 +1,9 @@
 insert into usuario values(06670061501, 'Pedro', 'bjkre@hotmail.com', '123456', 'rua 1', '99999999999');
-insert into usuario values(12345678, 'Diego', 'diego.bakugan@hotmail.com', '123456', 'rua 2', '92112534214');
-insert into usuario values(12345679, 'Pedritto', 'pedritto@hotmail.com', '123456', 'rua 3', '92112534214');
-insert into usuario values(12345698, 'Jorge', 'jorge@hotmail.com', '123456', 'rua 4', '82112534214');
+insert into usuario values(12086789572, 'Diego', 'diego.bakugan@hotmail.com', '123456', 'rua 2', '92112534214');
+insert into usuario values(49850359714, 'Pedritto', 'pedritto@hotmail.com', '123456', 'rua 3', '92112534214');
+insert into usuario values(82361487004, 'Jorge', 'jorge@hotmail.com', '123456', 'rua 4', '82112534214');
 
 insert into administrador values(06670061501);
-insert into profissional values(12345678, 06670061501, 'registro');
-insert into paciente values(12345679);
-insert into padrinho values(12345698, 12345678, 'UFS', '202000');
+insert into profissional values(12086789572, 06670061501, 'registro');
+insert into paciente values(49850359714);
+insert into padrinho values(82361487004, ,12086789572 'UFS', '20200014567');
